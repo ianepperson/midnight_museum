e02dbaecbe9c0524150b7656d120c6d73ed1717f
Ian Epperson <ian@epperson.com>
2022-07-17 13:22:14 -0700

    Add web server

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from argparse import ArgumentParser
-from time import sleep
+# from time import sleep
 import logging
+
+import flask
 
 from mqtt import MQTTHandler
 from effects import EffectsHandler
+from web_server import app
 
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-def go(mqtt_host, mqtt_port):
+def go(mqtt_host, mqtt_port, http_server_port):
     print('Starting MQTT handler...')
     mqtt = MQTTHandler(host=mqtt_host, port=mqtt_port)
     mqtt.start()
     print('Starting effects handler...')
     effects_handler = EffectsHandler(mqtt)
     effects_handler.start()
+
+    @app.before_request
+    def load_controls():
+        flask.g.mqtt = mqtt
+        flask.g.effects = effects_handler
+
     print('------------------------------')
     print('Running. Press ctrl-C to halt.')
     print('------------------------------')
     try:
-        while True:
-            sleep(1)
+        app.run(debug=True, port=http_server_port)
+        # while True:
+        #     sleep(1)
     except KeyboardInterrupt:
         print(' Shutdown requested')
     finally:
@@ -35,6 +45,13 @@ def go(mqtt_host, mqtt_port):
 if __name__ == '__main__':
     # Initialize parser
     parser = ArgumentParser()
+    parser.add_argument(
+        '--http_server_port',
+        metavar='PORT',
+        help='The port the web interface is served on',
+        type=int,
+        default=8080
+    )
     parser.add_argument(
         '--mqtt_host',
         metavar='HOST',
@@ -52,4 +69,4 @@ if __name__ == '__main__':
     # Grab the arguments
     args = parser.parse_args()
     # Run with it
-    go(args.mqtt_host, args.mqtt_port)
+    go(args.mqtt_host, args.mqtt_port, args.http_server_port)
